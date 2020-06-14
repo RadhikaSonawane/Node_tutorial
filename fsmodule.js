50050bf5577d07407c367946ba51e80ad9b10818
RadhikaSonawane <41570546+RadhikaSonawane@users.noreply.github.com>
2020-06-14 23:09:50 +0200

    first commit

---
+++ fsmodule.js
@@ -0,0 +1,2 @@
+const fs = require('fs');
+fs.copyFileSync("fileOne.txt", "fileTwo.txt");
---
+++ index.js
@@ -0,0 +1,11 @@
+
+const square = require('./square.js');
+
+const calsquare = (a) => {
+    console.log(`the value of a is ${a} and the area is` +square.area(a));
+
+    console.log(`the value of a is ${a} and the perimeter is` +square.perimeter(a))
+}
+
+console.log(__filename);
+console.log(__dirname);
