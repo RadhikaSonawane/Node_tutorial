1c9918d76273d76b4801e8ad2674553ffee6ded5
RadhikaSonawane <41570546+RadhikaSonawane@users.noreply.github.com>
2020-06-15 15:21:28 +0200

    server run html file

--- nodeServer_mini_pro/index.js
+++ nodeServer_mini_pro/index.js
@@ -1,13 +1,51 @@
-
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const hostname = "localhost";
 const port = 8000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content_Type','text/html');
-    res.end('<html> <body> <h1> server connect successfully </h1> </body> </html>')
+    //console.log(req.headers);
+    console.log('request for ' +req.url+ 'by method '+ req.method);
+    
+    if(req.method == 'GET'){
+        let fileURL;
+        if(req.url == '/'){
+            fileURL = "/index.html"
+        } else {
+            fileURL=req.url
+        }
+
+        let filePath = path.resolve('./public' + fileURL);
+        const fileExt = path.extname(filePath);
+
+        if (fileExt == '.html'){
+            fs.exists(filePath, (exists) => {
+                if(!exists){
+                    res.stausCode = 404;
+                    res.setHeader('Content-type', 'text/html');
+                    res.end('<html> <body> <h1> error 404: '+ fileUrl +' does not exist </h1> </body> </html>')
+                } 
+                    res.stausCode = 200;
+                    res.setHeader('Content-type', 'text/html');
+                    fs.createReadStream(filePath).pipe(res);
+            })
+        }
+            else{
+                res.stausCode = 404;
+                res.setHeader('Content-type', 'text/html');
+                res.end('<html> <body> <h1> error 404: '+ fileUrl +' not a HTML file </h1> </body> </html>');
+            }
+        }
+
+     else {
+        res.stausCode = 404;
+        res.setHeader('Content-type', 'text/html');
+        res.end('<html> <body> <h1> error 404: '+ fileUrl +' not supported </h1> </body> </html>')
+    }
+    //res.statusCode = 200;
+    //res.setHeader('Content_Type','text/html');
+    //res.end('<html> <body> <h1> server connect successfully </h1> </body> </html>')
 
 });
 
